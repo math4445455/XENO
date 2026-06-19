04e941c49172f4170359df33d2311bf4d52410db
Matheus Marques <matheusmlopes2024@gmail.com>
2026-06-19 08:03:13 -0300

    Atualizar o llm.py

--- XENO/llm.py
+++ XENO/llm.py
@@ -4,21 +4,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# O Render lerá GEMINI_API_KEY do painel Environment
 chave = os.getenv("GEMINI_API_KEY")
 if not chave:
     print("ERRO: GEMINI_API_KEY não encontrada!")
 
 genai.configure(api_key=chave)
 
-# Configuração do modelo Gemini
 model = genai.GenerativeModel(
     model_name="gemini-1.5-flash",
     system_instruction="Você é a XENO, uma IA educada, clara e objetiva que responde em português."
 )
 
 def gerar_resposta(mensagens):
-    # O app.py envia o histórico completo. Pegamos a última pergunta:
     pergunta_usuario = mensagens[-1]["content"]
     
     try:
